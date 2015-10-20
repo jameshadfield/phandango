dcfd0ee1f240e191643d6f9a4bbdc9738356a5a1
jameshadfield <jh22@sanger.ac.uk>
2015-10-20 15:43:43 +0100

    sorting now hardcoded

--- js/components/genomic/roary.parser.js
+++ js/components/genomic/roary.parser.js
@@ -25,7 +25,16 @@ function parseCSV(gff_string) {
 	// [11-...] genes
 
 
-	var splitLines = gff_string.split("\n").map(function(x){return x.replace(/"/g, '').trim().split(',')})
+	// var splitLines = gff_string.split("\n").map(function(x){return x.replace(/"/g, '').trim().split(',')})
+	var splitLines = gff_string.split("\n").map(function(x){
+		return x.trim().split('","').map(function(y) {
+			return y.replace(/"/g, '')
+		})
+	});
+
+	// remove the last lines
+	splitLines = splitLines.slice(0,-1)
+
 	var maxColIdx = splitLines[0].length, maxRowIdx = splitLines.length;
 	// note that they're not all the same length due to CSV parsing difficulties (the last columns dissapear if empty etc)
 
@@ -50,21 +59,38 @@ function parseCSV(gff_string) {
 }
 
 function generateRoary(roary,header,geneLen) {
-	var blocks = makeBlocks(roary,geneLen);
-	var arrows = makeAnnotation(header,geneLen);
+	var orderedIdx = sortHeader(header);
+	var blocks = makeBlocks(roary,geneLen,orderedIdx);
+	var arrows = makeAnnotation(header,geneLen,orderedIdx);
 	var genomeLength = geneLen * header.length;
 	var yValues = generateYValues(blocks,genomeLength)
 	return([blocks,arrows,genomeLength,yValues]);
 };
 
+function sortHeader(header) {
+	// returns arroy of indicies which we use to process
+	// currently hardcoded to sort into fragments
+	var len = header.length;
+	var indices = new Array(len);
+	for (var i = 0; i < len; ++i) indices[i] = i;
+	indices.sort(function (a,b) { // a,b are indices in this case!
+		if (header[a].fragment===header[b].fragment) {
+			return parseInt(header[a].order) < parseInt(header[b].order) ? -1 : 1;
+		} else {
+			return parseInt(header[a].fragment) < parseInt(header[b].fragment) ? -1 : 1;
+		}
+	})
+	return indices;
+}
 
-function makeBlocks(roary,geneLen) {
+function makeBlocks(roary,geneLen,orderedIdx) {
 	var ret = [];
 	Object.keys(roary).forEach( function(taxa) {
 		// console.log("key of roary: ",taxa)
-		var maxIdx = roary[taxa].length, inBlock=false, blockOpen=0, blockClose=0, xPos=0;
-		for (var i=0; i<maxIdx-1; i++) {
-			if (roary[taxa][i]) { // i.e. gene "present"
+		var maxIdx = orderedIdx.length, inBlock=false, blockOpen=0, blockClose=0, xPos=0;
+		for (var i=0; i<orderedIdx.length-1; i++) {
+			idx = orderedIdx[i];
+			if (roary[taxa][idx]) { // i.e. gene "present"
 				if (!inBlock) { // but we wern't in a block --> open one
 					inBlock=true, blockOpen=xPos;
 				} // else we were in a block --> extend the block (nothing needed)
@@ -79,12 +105,12 @@ function makeBlocks(roary,geneLen) {
 			}
 			xPos += geneLen; // basically a counter
 		}
-		// final case (i==maxIdx-1 i.e. final column)
-		if (roary[taxa][maxIdx-1] && inBlock) { // still in block
+		// final case (idx = end of orderedIdx)
+		if (roary[taxa][orderedIdx[maxIdx-1]] && inBlock) { // still in block
 			ret.push(new gubbinsParser.Block(blockOpen,xPos+geneLen,[taxa],0,0,0,0))
-		} else if (roary[taxa][maxIdx-1] && !inBlock) { // wern't in block but now are
+		} else if (roary[taxa][orderedIdx[maxIdx-1]] && !inBlock) { // wern't in block but now are
 			ret.push(new gubbinsParser.Block(xPos,xPos+geneLen,[taxa],0,0,0,0))
-		} else if (!roary[taxa][maxIdx-1] && inBlock) { // were in block but now arent
+		} else if (!roary[taxa][orderedIdx[maxIdx-1]] && inBlock) { // were in block but now arent
 			ret.push(new gubbinsParser.Block(blockOpen,xPos,[taxa],0,0,0,0))
 		}
 	} );
@@ -105,27 +131,28 @@ function generateYValues(blocks,genomeLength) {
 
 
 
-function makeAnnotation(header,geneLen) {
+function makeAnnotation(header,geneLen,orderedIdx) {
 	var ret=[];
 
 	// adding fragment by fragment (+ strand)
-	var currentFragment = header[0].fragment, fragmentOpen=0, strand='+';
-	for (var i=1; i<header.length; i++) {
-		if (currentFragment!==header[i].fragment || i+1===header.length) { // save as a block :)
+	var currentFragment = header[orderedIdx[0]].fragment, fragmentOpen=0;
+	for (var i=1; i<orderedIdx.length; i++) {
+		idx = orderedIdx[i];
+
+		// F R A G M E N T S
+		if (currentFragment!==header[idx].fragment || idx+1===header.length) { // save as a block :)
+			// console.log("fragment change. FROM ",currentFragment,"TO",header[idx].fragment,"AT",(i+1)*geneLen)
 			var info="locus_tag=fragment "+currentFragment+";";
-			ret.push(new gffParser.Arrow(fragmentOpen,(i+1)*geneLen, strand,"#FFA500", "black", 2,info))
+			ret.push(new gffParser.Arrow(fragmentOpen,(i+1)*geneLen, '+', currentFragment%2===0?"#FFCC00":"#FF9900", "black", 2,info))
 			fragmentOpen = (i+1)*geneLen;
-			currentFragment = header[i].fragment;
+			currentFragment = header[idx].fragment;
 		}
+
+		// G E N E S
+		var info="locus_tag="+header[idx].name+";product="+header[idx].annotation;
+		ret.push(new gffParser.Arrow(i*geneLen,(i+1)*geneLen, '-',"#318DCC", "black", 1,info))
 	}
 
-	// adding gene by gene (i.e. lots of geneLen blocks)
-	var strand = '-';
-	for (var i=0; i<header.length; i++) {
-		// Arrow(featurestart, featureend, direction, fill, stroke, strokeWidth, info)
-		var info="locus_tag="+header[i].name+";product="+header[i].annotation;
-		ret.push(new gffParser.Arrow(i*geneLen,(i+1)*geneLen, strand,"#318DCC", "black", 1,info))
-	}
 	return ret;
 }
 
