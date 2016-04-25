2f0b3f04bb8c7e995cd8ce459e5b220ef917c2a4
jameshadfield <jh22@sanger.ac.uk>
2016-04-25 17:23:16 +0100

    removed animated logo from non webkit browsers

--- js/containers/landingPage.jsx
+++ js/containers/landingPage.jsx
@@ -20,7 +20,7 @@ export const LandingPage = React.createClass({
           (formerly known as <a href="https://jameshadfield.github.io/phandango/v02x.html">JScandy</a>)
         </h3>
 
-        <AnimatedLogo w={600} h={200} interval={2000}/>
+        <AnimatedLogo w={600} h={200} interval={2000} animate={!!navigator.userAgent.match(/WebKit/i)}/>
         <p/>
 
 
