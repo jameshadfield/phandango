bccfae0351512c67c3908cf42d457c23a1006c23
jameshadfield <jh22@sanger.ac.uk>
2016-04-27 14:52:58 +0100

    canvas2svg no longer exceeds max call stack size - breaks IE compatability

--- js/misc/canvas2svg.js
+++ js/misc/canvas2svg.js
@@ -420,9 +420,9 @@
 
         //IE search for a duplicate xmnls because they didn't implement setAttributeNS correctly
         xmlns = /xmlns="http:\/\/www\.w3\.org\/2000\/svg".+xmlns="http:\/\/www\.w3\.org\/2000\/svg/gi;
-        if(xmlns.test(serialized)) {
-            serialized = serialized.replace('xmlns="http://www.w3.org/2000/svg','xmlns:xlink="http://www.w3.org/1999/xlink');
-        }
+        // if(xmlns.test(serialized)) {
+        //     serialized = serialized.replace('xmlns="http://www.w3.org/2000/svg','xmlns:xlink="http://www.w3.org/1999/xlink');
+        // }
 
         if(fixNamedEntities) {
             keys = Object.keys(namedEntities);
