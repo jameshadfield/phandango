8a49b00a81f918d7aead616e092d1975865903dc
james hadfield <jh22@sanger.ac.uk>
2017-11-10 10:52:06 -0800

    react router (browser message broken)

--- js/containers/app.jsx
+++ js/containers/app.jsx
@@ -1,64 +1,55 @@
-import "babel-polyfill";
+import 'babel-polyfill';
 import '../../css/JScandy.css';
 import '../../node_modules/flexboxgrid/css/flexboxgrid.min.css';
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import configureStore from '../store/configureStore';
 import DevTools from '../containers/devTools';
 import { Provider, connect } from 'react-redux';
 import { MainReactElement } from './main';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { UnsupportedBrowser } from '../components/UnsupportedBrowser';
-import { getBrowser } from '../misc/helperFunctions';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 /*  to fix iOS's dreaded 300ms tap delay, we need this plugin
-NOTE Facebook is not planning on supporting tap events (#436)
-because browsers are fixing/removing the click delay.
-Unfortunately it will take a lot of time before all mobile
+NOTE Facebook is not planning on supporting tap events (#436) because browsers are fixing/removing
+the click delay. Unfortunately it will take a lot of time before all mobile
 browsers (including iOS' UIWebView) will and can be updated.
 https://github.com/zilverline/react-tap-event-plugin
-*/
-injectTapEventPlugin();
+Following https://github.com/zilverline/react-tap-event-plugin/issues/61
+we wrap this in a try-catch as hotloading triggers errors */
+try {
+  injectTapEventPlugin();
+} catch (e) {
+  // empty
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.React = React; // for react chrome extension debugger
 }
 
-const browser = getBrowser();
-console.log(browser);
-let elements;
+const store = configureStore();
+const ConnectedMainReactElement = connect((state)=>({
+  page: state.router,
+  spinner: state.spinner,
+}))(MainReactElement);
 
-if (browser.mobile) {
-  window.ga('send', 'pageview', '/unsupportedBrowser');
-  elements = <UnsupportedBrowser msg="mobile devices are not yet supported"/>;
-} else if (browser.name === 'Firefox' && browser.version < 45) {
-  window.ga('send', 'pageview', '/unsupportedBrowser');
-  const msg = 'Firefox is only supported for versions 45 and above (you have version ' + browser.version.toString() + ')';
-  elements = <UnsupportedBrowser msg={msg}/>;
-} else {
-  const store = configureStore();
-  const ConnectedMainReactElement = connect((state)=>({
-    page: state.router,
-    spinner: state.spinner,
-  }))(MainReactElement);
-
-  let browserMessage = null;
-  if ([ 'Chrome', 'Safari', 'Firefox' ].indexOf((browser.name)) === -1) {
-    browserMessage = browser.name + ' version ' + browser.version.toString();
-  }
-  elements = [
-    <Provider store={store} key={'providerKey'}>
+const elements = [
+  <Provider store={store} key={'providerKey'}>
+    <BrowserRouter>
       <MuiThemeProvider>
-        <ConnectedMainReactElement browserMessage={browserMessage}/>
+        <Switch>
+          <Route path="/*" component={ConnectedMainReactElement}/>
+          {/* <ConnectedMainReactElement browserMessage={browserMessage}/> */}
+        </Switch>
       </MuiThemeProvider>
-    </Provider>,
-  ];
-  if (process.env.NODE_ENV !== 'production') {
-    elements.push(
-      <DevTools key={'devToolsKey'} store={store}/>
-    );
-  }
+    </BrowserRouter>
+  </Provider>,
+];
+if (process.env.NODE_ENV !== 'production') {
+  elements.push(
+    <DevTools key={'devToolsKey'} store={store}/>
+  );
 }
 
 // if (navigator.userAgent.match(/WebKit/i)) {
